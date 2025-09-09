6370f6dc0cb07950ec06625127f6d92c7e556a41
Henrique José <henrisilva2003@gmail.com>
2025-09-08 22:32:20 -0300

    Método findAll

--- src/main/java/br/senac/tads/dsw/dadospessoais/PessoaService.java
+++ src/main/java/br/senac/tads/dsw/dadospessoais/PessoaService.java
@@ -1,6 +1,7 @@
 package br.senac.tads.dsw.dadospessoais;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,35 +13,14 @@ public class PessoaService {
     public PessoaService(){
         mapPessoas = new HashMap<>();
 
-        mapPessoas.put("Henrique", new PessoaDto(
-                "Henrique",
-                "Henrique José",
-                "henrisilva2003@gmail.com",
-                LocalDate.parse("2003-06-03"),
-                "11 99999-9999",
-                List.of("Java", "Spring Boot"),
-                "123"));
+        mapPessoas.put("Henrique", new PessoaDto("Henrique","Henrique José","henrisilva2003@gmail.com",LocalDate.parse("2003-06-03"),"11 99999-9999",List.of("Java", "Spring Boot"),"123"));
 
-        mapPessoas.put("Alexandrino", new PessoaDto(
-                "Alexandrino",
-                "Ryan Alexandrino",
-                "ryansoftdev@gmail.com",
-                LocalDate.parse("2002-11-25"),
-                "11 99999-9999",
-                List.of("Python", "PyCharm"),
-                "123"));
+        mapPessoas.put("Alexandrino", new PessoaDto("Alexandrino","Ryan Alexandrino","ryansoftdev@gmail.com",LocalDate.parse("2002-11-25"),"11 99999-9999",List.of("Python", "PyCharm"),"123"));
 
-        mapPessoas.put("Lucas", new PessoaDto(
-                "Lucas",
-                "Lucas Beltrao",
-                "lucasgabriel405@gmail.com",
-                LocalDate.parse("2001-12-31"),
-                "11 99999-9999",
-                List.of("Java", "SQL"),
-                "123"));
+        mapPessoas.put("Lucas", new PessoaDto("Lucas","Lucas Beltrao","lucasgabriel405@gmail.com",LocalDate.parse("2001-12-31"),"11 99999-9999", List.of("Java", "SQL"),"123"));
     }
     public List<PessoaDto> findAll(){
-        return null;
+        return new ArrayList<>(mapPessoas.values());
     }
 
     public PessoaDto findByUsername(String username){
