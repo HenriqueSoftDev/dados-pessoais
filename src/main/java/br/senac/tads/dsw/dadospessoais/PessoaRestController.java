188b1e68efa39d9fe859295b682a59fd903f15eb
Henrique José <henrisilva2003@gmail.com>
2025-09-22 15:13:35 -0300

    Implementação da validação, PUT e DELETE

--- src/main/java/br/senac/tads/dsw/dadospessoais/PessoaRestController.java
+++ src/main/java/br/senac/tads/dsw/dadospessoais/PessoaRestController.java
@@ -1,15 +1,18 @@
 package br.senac.tads.dsw.dadospessoais;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Map;
 
 @RestController //Caso utilize a anotação @Controller, precisaria complementar as classes com o @ResponseBody
+@RequestMapping("/pessoas")
 public class PessoaRestController {
 
     @Autowired
@@ -22,7 +25,7 @@ public class PessoaRestController {
 
     //Passando os valores diretamente na URL
     @GetMapping("/apelido")
-    public PessoaDto findByUsernameRequestParam(@RequestParam(value = "username", required = false) String u){
+    public PessoaDto findByUsernameRequestParam(@RequestParam(value = "username", defaultValue = "teste") String u){
         return service.findByUsername(u);
     }
 
@@ -38,11 +41,32 @@ public class PessoaRestController {
     }
 
     @PostMapping
-    public ResponseEntity<PessoaDto> addNew(@RequestBody PessoaDto input){
+    public ResponseEntity<PessoaDto> addNew(@RequestBody PessoaDto input) {
         PessoaDto pessoa = service.addNew(input);
 
-        //TODO: CRIAR URI
-        return ResponseEntity.created(null).build();
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{username}")
+                .buildAndExpand(pessoa.getUsername())
+                .toUri();
+        return ResponseEntity.created(location).build();
 
+    }
+
+    @PutMapping("/{username}")
+    public ResponseEntity<PessoaDto> update(@PathVariable String username,
+                                            @RequestBody @Valid PessoaDto input) {
+        PessoaDto pessoa = service.findByUsername(username);
+        if (pessoa == null) {
+            return ResponseEntity.notFound().build();
+        }
+        service.update(username, input);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<Void> delete(@PathVariable String username) {
+        service.delete(username);
+        return ResponseEntity.noContent().build();
     }
 }
